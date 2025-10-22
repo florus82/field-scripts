385eb4fccc605ee9630482fadfd4c8cb50cf0041
florus82 <flosanfun@hotmail.com>
2025-10-22 08:39:14 +0000

    2025-10-22

--- Workflow/03_Derivate_Mask_from_IACS_and_make_MTSK.py
+++ Workflow/03_Derivate_Mask_from_IACS_and_make_MTSK.py
@@ -17,10 +17,11 @@ exclude_list = ['afforestation_reforestation',
                 'tree_wood_forest', 
                 'unmaintained']
 
-spat_temp_id = 'BRB-2023'
+year = 2023
+spat_temp_id = f'BRB-{year}'
 
 polygon_path = f'/data/{origin}fields/IACS/1_Polygons/GSA-DE_{spat_temp_id}.geoparquet'
-vrt_path = f'/data/fields/Auxiliary/vrt/Force_X_from_64_to_73_Y_from_39_to_47//Force_X_from_64_to_73_Y_from_39_to_47_0.vrt'
+vrt_path = f'/data/{origin}fields/Auxiliary/vrt/{year}/Force_X_from_64_to_73_Y_from_39_to_47//Force_X_from_64_to_73_Y_from_39_to_47_0.vrt'
 # convert lines to polyongs
 polygons_to_lines(polygon_path,
                   f'/data/{origin}fields/IACS/2_Lines/GSA-DE_{spat_temp_id}.gpkg',
@@ -29,30 +30,30 @@ polygons_to_lines(polygon_path,
 # rasterize lines
 rasterize_lines(f'/data/{origin}fields/IACS/2_Lines/GSA-DE_{spat_temp_id}.gpkg', 
                 vrt_path, 
-                f'/data/{origin}fields/IACS/3_Rasterized_lines/GSA-DE_{spat_temp_id}_lines_touch_true.tif', all_touch=True)
+                f'/data/{origin}fields/IACS/3_Rasterized_lines/{year}/GSA-DE_{spat_temp_id}_lines_touch_true.tif', all_touch=True)
 
 rasterize_lines(f'/data/{origin}fields/IACS/2_Lines/GSA-DE_{spat_temp_id}.gpkg', 
                 vrt_path, 
-                f'/data/{origin}fields/IACS/3_Rasterized_lines/GSA-DE_{spat_temp_id}_lines_touch_false.tif', all_touch=False)
+                f'/data/{origin}fields/IACS/3_Rasterized_lines/{year}/GSA-DE_{spat_temp_id}_lines_touch_false.tif', all_touch=False)
 
 # make a crop mask
 make_crop_mask(polygon_path, 
-               f'/data/{origin}fields/IACS/3_Rasterized_lines/GSA-DE_{spat_temp_id}_lines_touch_true.tif', 
+               f'/data/{origin}fields/IACS/3_Rasterized_lines/{year}/GSA-DE_{spat_temp_id}_lines_touch_true.tif', 
                vrt_path, 
-               f'/data/{origin}fields/IACS/4_Crop_mask/GSA-DE_{spat_temp_id}_cropMask_lines_touch_true.tif')
+               f'/data/{origin}fields/IACS/4_Crop_mask/{year}/GSA-DE_{spat_temp_id}_cropMask_lines_touch_true.tif')
 
 make_crop_mask(polygon_path, 
-               f'/data/{origin}fields/IACS/3_Rasterized_lines/GSA-DE_{spat_temp_id}_lines_touch_false.tif', 
+               f'/data/{origin}fields/IACS/3_Rasterized_lines/{year}/GSA-DE_{spat_temp_id}_lines_touch_false.tif', 
                vrt_path, 
-               f'/data/{origin}fields/IACS/4_Crop_mask/GSA-DE_{spat_temp_id}_cropMask_lines_touch_false.tif')
+               f'/data/{origin}fields/IACS/4_Crop_mask/{year}/GSA-DE_{spat_temp_id}_cropMask_lines_touch_false.tif')
 
 
 
-# This should happen at tile level, when we use S2 to finetune model --> labels needed for training
-# rasterized lines to 3D label
-print('take the long way')
-make_multitask_labels(f'/data/{origin}fields/IACS/3_Rasterized_lines/GSA-DE_{spat_temp_id}_lines_touch_true.tif', 
-                      f'/data/{origin}fields/IACS/5_Multitask_labels/GSA-DE_{spat_temp_id}_lines_touch_true_MTSK.tif')
+# # This should happen at tile level, when we use S2 to finetune model --> labels needed for training
+# # rasterized lines to 3D label
+# print('take the long way')
+# make_multitask_labels(f'/data/{origin}fields/IACS/3_Rasterized_lines/GSA-DE_{spat_temp_id}_lines_touch_true.tif', 
+#                       f'/data/{origin}fields/IACS/5_Multitask_labels/GSA-DE_{spat_temp_id}_lines_touch_true_MTSK.tif')
 
-make_multitask_labels(f'/data/{origin}fields/IACS/3_Rasterized_lines/GSA-DE_{spat_temp_id}_lines_touch_false.tif', 
-                       f'/data/{origin}fields/IACS/5_Multitask_labels/GSA-DE_{spat_temp_id}_lines_touch_false_MTSK.tif')
+# make_multitask_labels(f'/data/{origin}fields/IACS/3_Rasterized_lines/GSA-DE_{spat_temp_id}_lines_touch_false.tif', 
+#                        f'/data/{origin}fields/IACS/5_Multitask_labels/GSA-DE_{spat_temp_id}_lines_touch_false_MTSK.tif')
--- Workflow/04_Prepare_Predict_Postprocess_FORCE.py
+++ Workflow/04_Prepare_Predict_Postprocess_FORCE.py
@@ -1,47 +1,66 @@
 import sys
+import os
 sys.path.append('/home/potzschf/repos/')
 from helperToolz.helpsters import *
 
+# sys.path.append('/media/helperToolz')
+
+# from helpsters import *
 
 ####################################################### Prepare
 
+origin = 'Docker'
+
+if origin == 'Docker':
+    prefix = '/home/'
+else:
+    prefix = '/data/Aldhani/eoagritwin/'
+
 # make vrts from force outputs for easier processing
-year = 2019
-reduced_files = reduce_forceTSA_output_to_validmonths(f'/data/Aldhani/eoagritwin/force/output/BRANDENBURG/{year}/', 3, 8)
-ordered_files = getCOLORSinOrderFORCELIST(reduced_files, ['BLU', 'GRN', 'RED', 'BRN'])
-force_to_vrt(reduced_files, ordered_files, f'/data/Aldhani/eoagritwin/fields/Auxiliary/vrt/{year}_2/', True, bandnames=['BLU', 'GRN', 'RED', 'BRN'])
+year = 2023
+reduced_files = reduce_forceTSA_output_to_validmonths(f'{prefix}force/output/BRANDENBURG/{year}/', 3, 8)
+ordered_files = force_order_Colors_for_VRT(reduced_files, ['BLU', 'GRN', 'RED', 'BNR'], [f'MONTH-{d:02d}' for d in range(3,9,1)])
+
+vrt_out = f'{prefix}fields/Auxiliary/vrt/{year}/'
+if len(getFilelist(f'{vrt_out}', '.vrt', deep=True)) > 0:
+    print('VRT seems to be already computated, probably to create masks based on IACS')
+else:
+    force_to_vrt(reduced_files, ordered_files, vrt_out, True, bandnames=['BLU', 'GRN', 'RED', 'BNR'])
 
 # # load vrts into npdstack
-# dat = loadVRTintoNumpyAI4(f'/data/fields/Auxiliary/vrt/{year}/{getFORCExyRangeName(get_forcetiles_range(reduced_files))}')
+print('Load vrt into numpy for prediction')
+dat = loadVRTintoNumpyAI4(f'{prefix}/fields/Auxiliary/vrt/{year}/{getFORCExyRangeName(get_forcetiles_range(reduced_files))}')
 
-# # set tiling scheme and chip size on which prediction will be undertaken
-# chipsize = 128*1 # 5 is the maximum with GPU in basement
-# overlap  = 20
+# set tiling scheme and chip size on which prediction will be undertaken
+chipsize = 128*2 # 5 is the maximum with GPU in basement
+overlap  = 20
 
-# row_col_ind = get_row_col_indices(chipsize, overlap, dat.shape[2:][0], dat.shape[2:][1])
+row_col_ind = get_row_col_indices(chipsize, overlap, dat.shape[2:][0], dat.shape[2:][1])
 
-# ####################################################### Predict
+####################################################### Predict
+print('start prediction')
+predicted_chips_list = predict_on_GPU(f'{prefix}fields/output/models/model_state_All_but_LU_transformed_42.pth', row_col_ind, dat)
 
-# predicted_chips_list = predict_on_GPU('/data/fields/output/model_state_All_but_LU_transformed_42.pth', row_col_ind, dat)
-
-# ####################################################### Postprocess
-
-# # export the predicted chips (masked and not masked)
-# export_GPU_predictions(predicted_chips_list, 
-#                        '/data/fields/IACS/4_Crop_mask/GSA-DE_BRB-2019_cropMask_lines_touch_false_lines_touch_false_linecrop.tif', 
-#                        f'/data/fields/Auxiliary/vrt/{year}/{getFORCExyRangeName(get_forcetiles_range(reduced_files))}',row_col_ind, 
-#                        '/data/fields/output/predictions/FORCE/BRANDENBURG/{year}/')
-
-
-# # make vrt of predicted image chips
-# for chip in ['chips/', 'masked_chips/']:
-#     predicted_chips_to_vrt(f'/data/fields/output/predictions/FORCE/BRANDENBURG/{year}/{chip}', chipsize, overlap,
-#                            '/data/fields/output/predictions/FORCE/BRANDENBURG/vrt/{year}/', pyramids=True)
+####################################################### Postprocess
+print('start exporting')
+# export the predicted chips (masked and not masked)
+export_GPU_predictions(predicted_chips_list, 
+                       f'{prefix}fields/IACS/4_Crop_mask/{year}/GSA-DE_BRB-{year}_cropMask_lines_touch_false_lines_touch_false_linecrop.tif', 
+                       f'{prefix}fields/Auxiliary/vrt/{year}/{getFORCExyRangeName(get_forcetiles_range(reduced_files))}',
+                       row_col_ind, 
+                       f'{prefix}fields/output/predictions/FORCE/BRANDENBURG/{year}/',
+                       chipsize, overlap)
 
 
-# # make a subset of the reference mask (extent FORCE output) to the extent of the prediction
-# subset_mask_to_prediction_extent('/data/Aldhani/eoagritwin/fields/IACS/4_Crop_mask/GSA-DE_BRB-2019_cropMask_lines_touch_false_lines_touch_false_linecrop.tif', 
-#                                  f'/data/Aldhani/eoagritwin/fields/output/predictions/FORCE/BRANDENBURG/vrt/{year}/{chipsize}_{overlap}_chips.vrt')
+# make vrt of predicted image chips
+for chip in ['chips/', 'masked_chips/']:
+    predicted_chips_to_vrt(f'{prefix}fields/output/predictions/FORCE/BRANDENBURG/{year}/{chip}', chipsize, overlap,
+                           f'{prefix}fields/output/predictions/FORCE/BRANDENBURG/vrt/{year}/', pyramids=True)
 
-# subset_mask_to_prediction_extent('/data/Aldhani/eoagritwin/fields/IACS/4_Crop_mask/GSA-DE_BRB-2019_cropMask_lines_touch_true_lines_touch_true_linecrop.tif', 
-#                                  f'/data/Aldhani/eoagritwin/fields/output/predictions/FORCE/BRANDENBURG/vrt/{year}/{chipsize}_{overlap}_chips.vrt')
+
+# make a subset of the reference mask (extent FORCE output) to the extent of the prediction
+subset_mask_to_prediction_extent(f'{prefix}fields/IACS/4_Crop_mask/{year}/GSA-DE_BRB-{year}_cropMask_lines_touch_false_lines_touch_false_linecrop.tif', 
+                                 f'{prefix}fields/output/predictions/FORCE/BRANDENBURG/{year}/vrt/{chipsize}_{overlap}_chips.vrt')
+
+subset_mask_to_prediction_extent(f'{prefix}fields/IACS/4_Crop_mask/{year}/GSA-DE_BRB-{year}_cropMask_lines_touch_true_lines_touch_true_linecrop.tif', 
+                                 f'/data/Aldhani/eoagritwin/fields/output/predictions/FORCE/BRANDENBURG/{year}/vrt/{chipsize}_{overlap}_chips.vrt')
